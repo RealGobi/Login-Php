c2b7113992ef5d7ae0f264071bef9ea847faeb6d
Jimmy Svensson <jimmy888swe@gmail.com>
2019-04-07 23:44:16 +0200

    emptyFeildFixREG

--- registration.php
+++ registration.php
@@ -6,7 +6,7 @@ try {
         $passwordInput2 = $_POST['password2'];
 
         if (empty($emailInput) || empty($passwordInput) || empty($passwordInput2)) {
-            header('Location:/Lab1/signup.php?error=emptyfeild=mail=');
+            header('Location:/Lab1/signup.php?error=emptyfeildReg');
             exit();
         }
         else if (!filter_var($emailInput, FILTER_VALIDATE_EMAIL)){
@@ -29,13 +29,10 @@ try {
             $openData = file_get_contents('json/registration.json');
             $fileData = json_decode($openData,true);
             $jsonArray = array('email'=>$emailInput, 'password'=> $bcryptedPassword);
-            // $fp = fopen('json/registration.json', 'a');
-            // fwrite($fp, json_encode($jsonArray));
-            // fclose($fp);
             $fileData[]= $jsonArray;
-            $putData = json_encode($fileData);
+            
+            $putData = json_encode($fileData);     
             file_put_contents('json/registration.json', $putData);
-
             header('Location:/Lab1/signup.php?signup=success');
             exit();
         }
--- signup.php
+++ signup.php
@@ -5,7 +5,7 @@ require 'header.php'; ?>
         <h1>Regestrera dig!</h1>
         <?php
         if(isset($_GET['error'])){
-            if($_GET['error'] == 'emptyfeild'){
+            if($_GET['error'] == 'emptyfeildReg'){
                 echo '<p style="color:red">Fyll i alla fält.</p>';
             }
             else if ($_GET['error'] == 'invalidEmail'){
@@ -13,6 +13,9 @@ require 'header.php'; ?>
             }
             else if ($_GET['error'] == 'passwordcheckfailed'){
                 echo '<p style="color:red">Oops! Lösenorden matchade inte, gö om gö rätt!.</p>';   
+            }
+            else if ($_GET['error'] == 'mailinuse'){
+                echo '<p style="color:red; font-size:.73rem; padding-top:0px;">Email-adressen finns redan i arkivet.</p>';  
             }
        
         }
@@ -29,7 +32,7 @@ require 'header.php'; ?>
         <input type="email" name="email" placeholder="Email...">
         <label>Lösenord:</label>
         <input type="password" name="password" minlength="5" placeholder="Lösenord...">
-        <input type="password" name="password2" minlength="5" placeholder="Ta lösenord igen...">
+        <input type="password" name="password2" minlength="5" placeholder="Ta lösenordet igen...">
         <button type="submit">Registrera dig!</button>
     </form>
     </main>
